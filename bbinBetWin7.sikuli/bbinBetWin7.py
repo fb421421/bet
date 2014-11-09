efd011ae1181a3c51af8561a6893a88d09d64b5d
fb421421 <fb421421@gmail.com>
2014-11-09 15:38:54 +0800

    add

--- bbinBetWin7.sikuli/bbinBetWin7.py
+++ bbinBetWin7.sikuli/bbinBetWin7.py
@@ -3,9 +3,9 @@ from datetime import datetime, date, time
 import logging
 import ConfigParser
 import smtplib
-
-config = ConfigParser.RawConfigParser()
-config.read('config.properties')
+import random
+import traceback
+import sys
 
 def send_email(content):
             gmail_user = "fb421421@gmail.com"
@@ -37,7 +37,6 @@ def writeProperty(config, name , value):
         config.write(configfile)
 
 def restart():
-    import random
     line=random.randint(1, 5)
     if line==1:
         click("1415471095183.png")
@@ -61,37 +60,34 @@ def restart():
     
     print "切换线路："+str(line)
     
-    
-
+#读取配置文件，设置日志系统    
+config = ConfigParser.RawConfigParser()
+config.read('config.properties')
 logging.basicConfig(filename='example.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%Y/%d/%m %H:%M:%S %p')
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')        
-
+       
+#禁止鼠标移动延时
 Settings.MoveMouseDelay = 0
-
 #赢的次数
 winCount=int(config.get('WinLoseCount', 'winCount'));
-
 #输的次数
 loseCount=int(config.get('WinLoseCount', 'loseCount'));
 
-print    winCount
-print    loseCount
-writeProperty(config,'winCount',str(winCount+1))
+logging.info("初始化参数winCount:"+str(winCount))
+logging.info("初始化参数loseCount:"+str(loseCount))
+
 
 #检查是否可以下注
 while(True):
     try:        
-        #wait("1415469765357.png",FOREVER)
+        hasIt=None#wait("1415469765357.png",3)
+        if not hasIt:
+           logging.info("等等开局结果符合下注条件超过3分钟，切换线路")     
+           restart()
         
-        
-        #hasStart=wait("1415456736137.png",10*60)
-
-        #if hasStart:
-           #print "开始下注"     
-        #else:
-            #restart()
+        hasStart=wait("1415456736137.png",10*60)
+        if not hasStart:
+           logging.info("等待开局失败，切换线路")     
+           restart()
         
         #选择筹码
         click("1415458560817.png")
@@ -133,37 +129,37 @@ while(True):
                 if exists("1415460987621.png",0):
                     winCount=winCount+1
                     loseCount=0
-                    print "结果：赢"
+                    logging.info("结果：赢")
                 elif exists("1415460966925.png",0):
                     loseCount=loseCount+1
-                    print "结果：输"
+                    logging.info("结果：输")
                 break
             elif exists("1415456736137.png",0):
-                print "结果：和"
+                logging.info("结果：和")
                 break
            
             now = datetime.now()
             pastSeconds=(now-past).seconds
             if pastSeconds>60*10:
-                print "等待结算时间过长："+str(pastSeconds)
+                logging.info("等待结算时间过长："+str(pastSeconds))
                 restart()
             
-            
-    
        
         #处理结果
-        print  "赢:"
-        print  winCount
-        print  "输:"
-        print  loseCount
+        writeProperty(config,'winCount',str(winCount))
+        writeProperty(config,'loseCount',str(loseCount))
+        logging.info("赢:"+str(winCount))
+        logging.info("输:"+str(loseCount))
         
         if winCount==100:
             send_email("success!")
             break;
         
         #返回第一步重新开始
-    except e:
-        print e
+    except:
+        logging.error("error")
+        logging.error(traceback.format_exc())
+        
 
     
 send_email("failed!")
