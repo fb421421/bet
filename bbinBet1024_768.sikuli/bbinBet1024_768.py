08a91a48c35ef6a82c89e80c5c7b5f24c6f80733
fb421421 <fb421421@gmail.com>
2014-11-10 17:56:14 +0800

    gg

--- bbinBet1024_768.sikuli/bbinBet1024_768.py
+++ bbinBet1024_768.sikuli/bbinBet1024_768.py
@@ -1,4 +1,4 @@
-#屏幕 1366*678 windows7 包裹浏览器地址栏，和下面桌面菜单栏
+#屏幕 1024*768 windows 包裹浏览器地址栏，和下面桌面菜单栏
 from datetime import datetime, date, time
 import logging
 import ConfigParser
@@ -39,23 +39,23 @@ def writeProperty(config, name , value):
 def restart():
     line=random.randint(1, 5)
     if line==1:
-         Region(256,571,202,66).inside().click("1415471095183.png")
+         Region(74,559,217,73).inside().click("1415612542553.png")
     elif line==2:
-         Region(256,571,202,66).inside().click("1415471137742.png")
+         Region(74,559,217,73).inside().click("1415612550712.png")
     elif line==3:
-         Region(256,571,202,66).inside().click("1415471166215.png")
+         Region(74,559,217,73).inside().click("1415612559209.png")
     elif line==4:
-         Region(256,571,202,66).inside().click("1415471189433.png")
+         Region(74,559,217,73).inside().click("1415612567288.png")
     elif line==5:
-         Region(256,571,202,66).inside().click("1415471206482.png")
+         Region(74,559,217,73).inside().click("1415557058484.png")
         
     wait(10)
-    Region(300,538,114,49).inside().click("1415472178524.png")
+    Region(96,540,152,41).inside().click("1415613249853.png")
     
     
     
     wait(10)
-    Region(297,529,117,60).inside().click("1415472205466.png")
+    Region(96,540,152,41).inside().click("1415613268638.png")
     
     
     logging.info("切换线路："+str(line))
@@ -66,13 +66,16 @@ config.read('config.properties')
 logging.basicConfig(filename='example.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%Y/%d/%m %H:%M:%S %p')
        
 #禁止鼠标移动延时
-Settings.MoveMouseDelay = 0
+Settings.MoveMouseDelay =0.5
+Settings.SlowMotionDelay=0.5
+
 #赢的次数
 winCount=int(config.get('WinLoseCount', 'winCount'));
 #输的次数
 loseCount=int(config.get('WinLoseCount', 'loseCount'));
 name=config.get('WinLoseCount', 'name')
 
+logging.info("开始新的应用")
 logging.info("初始化参数winCount:"+str(winCount))
 logging.info("初始化参数loseCount:"+str(loseCount))
 
@@ -80,29 +83,41 @@ logging.info("初始化参数loseCount:"+str(loseCount))
 #检查是否可以下注
 while(True):
     try:
-
+        logging.info("----------------------------")
         logging.info("检查是否符合开局结果")
-
-        #try:
-           # Region(210,613,419,108).inside().wait("1415469765357.png",60*10)
-        #except:
-            #logging.info("等待结算时间过长")
-            #restart()
+        past = datetime.now()
+        while(True):
+                if Region(724,280,26,43).inside().exists(Pattern("1415613202680.png").similar(0.80),3):
+                    break
+                
+                  
+                elif Region(731,282,19,39).inside().exists(Pattern("1415612060844.png").similar(0.80),0):
+                    break
+               
+                now = datetime.now()
+                pastSeconds=(now-past).seconds
+                if pastSeconds>60*3:
+                    logging.info("等待结算时间过长")
+                    loseCount=0
+                    break
         
         
         logging.info("等待下注")
-        hasStart=Region(760,413,392,265).inside().wait("1415456736137.png",10*60)
-        if not hasStart:
+        try:
+           Region(698,456,78,141).inside().wait("1415613310774.png",10*60)
+        except:     
            logging.info("等待开局失败，切换线路")     
            restart()
         
         #选择筹码
         logging.info("选择筹码")
-        Region(467,503,373,111).inside().click("1415458560817.png")
+        Region(375,511,197,122).inside().click("1415612313699.png")
+       
     
         #下注
         logging.info("下注")
-        Region(328,362,236,226).inside().hover("1415459252324.png")
+        Region(161,356,213,208).inside().hover("1415612375892.png")
+      
         
         betCount=0
         if loseCount==0:
@@ -110,10 +125,12 @@ while(True):
             mouseUp()
         else:
             #规避风险，可以多翻一倍
-            if(loseCount>=1):
-                betCount=loseCount-1
-            else:
-                betCount=loseCount;
+            #if(loseCount>=1):
+             #   betCount=loseCount-1
+             #   if loseCount==1:
+              #      winCount=winCount-1
+            #else:
+            betCount=loseCount
                 
             for x in range(2**betCount):
                 wait(0.1)
@@ -122,12 +139,12 @@ while(True):
     
         #点击确认下注
         logging.info("确认下注："+str(2**betCount))
-        Region(838,438,288,195).inside().click("1415456736137.png")
+        Region(698,456,78,141).inside().click("1415613310774.png")
 
 
         logging.info("等待下注结束")
     
-        hasStop=Region(838,438,288,195).inside().waitVanish("1415456736137.png",60*3)
+        hasStop=Region(698,456,78,141).inside().waitVanish("1415613310774.png",60*3)
         if not hasStop:
            logging.info("等待下注失败，切换线路")     
            restart()
@@ -136,19 +153,18 @@ while(True):
         logging.info("等待发牌结束")
         past = datetime.now()
         while(True):
-            sleep(0.1)
-            if Region(472,431,456,190).inside().exists("1415459624895.png",0):
+            if Region(342,452,338,113).inside().exists("1415612644698.png",0):
             
                 #统计输赢次数
-                if Region(466,432,445,190).inside().exists("1415460987621.png",0):
+                if Region(342,452,338,113).inside().exists("1415612769689.png",0):
                     winCount=winCount+1
                     loseCount=0
                     logging.info("结果：赢")
-                elif Region(466,432,445,190).inside().exists("1415460966925.png",0):
+                elif Region(342,452,338,113).inside().exists("1415613137637.png",0):
                     loseCount=loseCount+1
                     logging.info("结果：输")
                 break
-            elif Region(786,416,359,266).inside().exists("1415456736137.png",0):
+            elif Region(698,456,78,141).inside().exists("1415613310774.png",0):
                 logging.info("结果：和")
                 break
            
@@ -164,11 +180,28 @@ while(True):
         writeProperty(config,'loseCount',str(loseCount))
         logging.info("赢:"+str(winCount))
         logging.info("输:"+str(loseCount))
+        logging.info("----------------------------")
+
+        #最多翻7次
+        if loseCount>7:
+            loseCount=0;
         
         if winCount==100:
             logging.info("恭喜你，今天任务完成！")
-            send_email("恭喜你"+name+"，今天任务完成！")
-            break;
+            send_email("恭喜你"+name+"，今天任务完成:"+str(winCount))
+       
+        elif winCount==200:
+            logging.info("恭喜你，今天任务完成！")
+            send_email("恭喜你"+name+"，今天任务完成:"+str(winCount))
+
+        elif winCount==500:
+            logging.info("恭喜你，今天任务完成！")
+            send_email("恭喜你"+name+"，今天任务完成:"+str(winCount))
+
+        elif winCount>=1000:
+            logging.info("恭喜你，今天任务完成！")
+            send_email("恭喜你"+name+"，今天任务完成:"+str(winCount))
+            exit(1)
         
         #返回第一步重新开始
     except:
--- bbinBet1440_900.sikuli/bbinBet1440_900.py
+++ bbinBet1440_900.sikuli/bbinBet1440_900.py
@@ -1,4 +1,4 @@
-#屏幕 1366*678 windows7 包裹浏览器地址栏，和下面桌面菜单栏
+#屏幕 1440*900 windows7 包裹浏览器地址栏，和下面桌面菜单栏
 from datetime import datetime, date, time
 import logging
 import ConfigParser
@@ -39,23 +39,23 @@ def writeProperty(config, name , value):
 def restart():
     line=random.randint(1, 5)
     if line==1:
-         Region(256,571,202,66).inside().click("1415471095183.png")
+         Region(186,607,287,186).inside().click("1415556959371.png")
     elif line==2:
-         Region(256,571,202,66).inside().click("1415471137742.png")
+         Region(186,607,287,186).inside().click("1415556982408.png")
     elif line==3:
-         Region(256,571,202,66).inside().click("1415471166215.png")
+         Region(186,607,287,186).inside().click("1415557005501.png")
     elif line==4:
-         Region(256,571,202,66).inside().click("1415471189433.png")
+         Region(186,607,287,186).inside().click("1415557039003.png")
     elif line==5:
-         Region(256,571,202,66).inside().click("1415471206482.png")
+         Region(186,607,287,186).inside().click("1415557058484.png")
         
     wait(10)
-    Region(300,538,114,49).inside().click("1415472178524.png")
+    Region(339,621,68,70).inside().click("1415557394923.png")
     
     
     
     wait(10)
-    Region(297,529,117,60).inside().click("1415472205466.png")
+    Region(339,621,68,70).inside().click("1415557434527.png")
     
     
     logging.info("切换线路："+str(line))
@@ -66,7 +66,9 @@ config.read('config.properties')
 logging.basicConfig(filename='example.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%Y/%d/%m %H:%M:%S %p')
        
 #禁止鼠标移动延时
-Settings.MoveMouseDelay = 0
+Settings.MoveMouseDelay =0.5
+Settings.SlowMotionDelay=0.5
+
 #赢的次数
 winCount=int(config.get('WinLoseCount', 'winCount'));
 #输的次数
@@ -78,31 +80,44 @@ logging.info("初始化参数loseCount:"+str(loseCount))
 
 
 #检查是否可以下注
+flag=0
 while(True):
     try:
 
         logging.info("检查是否符合开局结果")
-
-        #try:
-           # Region(210,613,419,108).inside().wait("1415469765357.png",60*10)
-        #except:
-            #logging.info("等待结算时间过长")
-            #restart()
+        past = datetime.now()
+        while(True):
+                if Region(960,321,43,57).inside().exists(Pattern("1415595384222.png").similar(0.80),3):
+                    break
+                
+                  
+                elif Region(960,321,43,57).inside().exists(Pattern("1415596000971.png").similar(0.80),0):
+                    break
+               
+                now = datetime.now()
+                pastSeconds=(now-past).seconds
+                if pastSeconds>60*3:
+                    logging.info("等待结算时间过长")
+                    loseCount=0
+                    break
         
         
         logging.info("等待下注")
-        hasStart=Region(760,413,392,265).inside().wait("1415456736137.png",10*60)
-        if not hasStart:
+        try:
+           Region(898,548,212,212).inside().wait("1415556232362.png",10*60)
+        except:     
            logging.info("等待开局失败，切换线路")     
            restart()
         
         #选择筹码
         logging.info("选择筹码")
-        Region(467,503,373,111).inside().click("1415458560817.png")
+        Region(445,574,471,153).inside().click("1415556359027.png")
+       
     
         #下注
         logging.info("下注")
-        Region(328,362,236,226).inside().hover("1415459252324.png")
+        Region(337,453,189,168).inside().hover("1415556570732.png")
+      
         
         betCount=0
         if loseCount==0:
@@ -110,10 +125,12 @@ while(True):
             mouseUp()
         else:
             #规避风险，可以多翻一倍
-            if(loseCount>=1):
-                betCount=loseCount-1
-            else:
-                betCount=loseCount;
+            #if(loseCount>=1):
+             #   betCount=loseCount-1
+             #   if loseCount==1:
+              #      winCount=winCount-1
+            #else:
+            betCount=loseCount
                 
             for x in range(2**betCount):
                 wait(0.1)
@@ -122,12 +139,12 @@ while(True):
     
         #点击确认下注
         logging.info("确认下注："+str(2**betCount))
-        Region(838,438,288,195).inside().click("1415456736137.png")
+        Region(898,548,212,212).inside().click("1415556232362.png")
 
 
         logging.info("等待下注结束")
     
-        hasStop=Region(838,438,288,195).inside().waitVanish("1415456736137.png",60*3)
+        hasStop=Region(898,548,212,212).inside().waitVanish("1415556232362.png",60*3)
         if not hasStop:
            logging.info("等待下注失败，切换线路")     
            restart()
@@ -136,19 +153,18 @@ while(True):
         logging.info("等待发牌结束")
         past = datetime.now()
         while(True):
-            sleep(0.1)
-            if Region(472,431,456,190).inside().exists("1415459624895.png",0):
+            if Region(464,488,523,267).inside().exists("1415556682240.png",0):
             
                 #统计输赢次数
-                if Region(466,432,445,190).inside().exists("1415460987621.png",0):
+                if Region(464,488,523,267).inside().exists("1415556801768.png",0):
                     winCount=winCount+1
                     loseCount=0
                     logging.info("结果：赢")
-                elif Region(466,432,445,190).inside().exists("1415460966925.png",0):
+                elif Region(464,488,523,267).inside().exists("1415557766219.png",0):
                     loseCount=loseCount+1
                     logging.info("结果：输")
                 break
-            elif Region(786,416,359,266).inside().exists("1415456736137.png",0):
+            elif Region(898,548,212,212).inside().exists("1415556232362.png",0):
                 logging.info("结果：和")
                 break
            
@@ -164,6 +180,9 @@ while(True):
         writeProperty(config,'loseCount',str(loseCount))
         logging.info("赢:"+str(winCount))
         logging.info("输:"+str(loseCount))
+
+        if loseCount>7:
+            loseCount=0;
         
         if winCount==100:
             logging.info("恭喜你，今天任务完成！")
